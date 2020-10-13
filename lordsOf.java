106caf6faf5cdfb077845c8c866b1e39dc335647
Tommi Ahonen <72734604+Hamburgaren@users.noreply.github.com>
2020-10-13 17:23:14 +0200

    Please fix your indentation!

--- lordsOf.java
+++ lordsOf.java
@@ -1,5 +1,5 @@
 public class lorsOf{
-public static void main(String[] args){
-system.out.println("hello boiz")
-}
+  public static void main(String[] args){
+    system.out.println("hello boiz")
+  }
 }
